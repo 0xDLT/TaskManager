cb715adbbae89a0a85fa35873d89607f25d8cb00
0xdlt <30773517+0xDLT@users.noreply.github.com>
2025-08-10 20:22:31 +0300

    📗add dachboard

---
+++ client/task-manager/src/components/layouts/DashboardLayout.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useContext } from 'react';
+import { UserContext } from '../../context/userContext';
+import Navbar from '../../components/layouts/Navbar';
+import SideMenu from '../../components/layouts/SideMenu';
+
+const DashboardLayout = ({children, activeMenu}) => {
+    const { user } = useContext(UserContext);
+  return (
+    <div className=''>
+        <Navbar activeMenu={activeMenu} />
+
+        {user && (
+          <div>
+            <div className="flex">
+              <div className="max-[1080px]:hidden" />
+              <SideMenu activeMenu={activeMenu} />
+            </div>
+            <div className="grow mx-5">{children}</div>
+          </div>
+        )}
+    </div>
+  );
+};
+
+export default DashboardLayout
